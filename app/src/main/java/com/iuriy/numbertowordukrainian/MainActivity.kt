04087142aedb2116b1eeba74e544a5bcf6e936e4
Luicha <iuriygomez@gmail.com>
2022-10-14 00:04:51 -0300

    Initial commit

--- app/src/main/java/com/iuriy/numbertowordukrainian/MainActivity.kt
+++ app/src/main/java/com/iuriy/numbertowordukrainian/MainActivity.kt
@@ -31,7 +31,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NumberToWordUkrainianTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize() ,
                     color = MaterialTheme.colors.background
